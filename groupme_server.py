d23bba4a6d31206abcd4b026080adcb43ce34169
Drew Siedel <dts.siedel@gmail.com>
2022-06-19 17:13:07 -0400

    Add logic for uploading image and responding in the chat

--- groupme_server.py
+++ groupme_server.py
@@ -15,6 +15,10 @@ ip_var = 'MY_IP'
 ip = os.getenv(ip_var)
 if not ip:
     raise Exception(f'Need to set {ip_var}.')
+token_var = 'GROUPME_BOT_TOKEN'
+token = os.getenv(token_var)
+if not token:
+    raise Exception(f'Need to set {token_var}.')
 server_var = 'DALLE_SERVER_ADDRESS'
 server = os.getenv(server_var)
 if not server:
@@ -25,14 +29,25 @@ dalle_url = f'{server}/dalle'
 APP = Flask(__name__)
 
 
-def send_msg(bot_id, msg):
+def send_msg(msg, image_url=None):
     url = 'https://api.groupme.com/v3/bots/post'
-    data = {'bot_id': bot_id, 'text': msg}
+    data = {'bot_id': bot_id, 'text': f'{msg}. Beep boop.'}
+    if image_url:
+        data['picture_url'] = image_url
+    print(f'Sending {data} to {url}')
     res = requests.post(url, json=data)
     print(res.text)
 
 
-def fetch_images(msg):
+def register_image(local_path):
+    image_url = 'https://image.groupme.com/pictures'
+    with open(local_path, 'rb') as image_binary:
+        res = requests.post(image_url, data=image_binary, params={'token':token})
+        print(res.status_code, res.text)
+    return res.json()['payload']['url']
+
+
+def fetch_images(msg, requester):
     print(f'Requesting from {dalle_url}')
     request = {
         'num_images': 1,
@@ -47,15 +62,24 @@ def fetch_images(msg):
     with open(f'image.png', 'wb') as fh:
         fh.write(base64.b64decode(image))
 
+    url = register_image('image.png')
+    print('uploaded result to', url)
+
+    send_msg(f'{msg}, requested by {requester}', url)
+
 
 @APP.route("/message", methods=['POST'])
 def on_message():
     print(request.json)
     message = request.json
     text = message['text']
-    callword = '!dalle'
-    if text.startswith(callword):
-        fetch_images(text[len(callword):])
+    if len(text) > 200:
+        send_msg('Text too long. Stop trying to break it, Tom (presumably).')
+    else:
+        callword = '!dalle'
+        requester = message['name']
+        if text.startswith(callword):
+            fetch_images(text[len(callword):], requester)
     return {'success': True}
 
 
